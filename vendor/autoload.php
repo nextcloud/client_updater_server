7fd3f4483304f2474e468b5261739c65a6b59eef
Arthur Schiwon <blizzz@arthur-schiwon.de>
2021-06-17 12:10:48 +0200

    update phpunit
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit90ffc5ab61d187781e6fedac71b76498::getLoader();
+return ComposerAutoloaderInitc0a637bd785126743dc992606b58e659::getLoader();
