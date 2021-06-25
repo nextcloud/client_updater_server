c55195eb772071c32ab667cbaec174b660386365
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2021-06-25 08:41:48 +0200

    require the Response php file instead of a removed one
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/src/Response.php';
 
 // Set Content-Type to XML
 header('Content-Type: application/xml');
