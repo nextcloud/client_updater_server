47ecf543a81c034be8fc815db7bcf905059fbed0
root <root@portal.portal.nextcloud.com>
2024-12-28 04:01:15 +0100

    Update daily Linux to 20241228
    Signed-off-by: root <root@portal.portal.nextcloud.com>

--- config/config.php
+++ config/config.php
@@ -15,7 +15,7 @@ $betaReleaseDate = '2024-09-09 17:00';
 $betaVersionInternal = '3.13.83';
 $betaVersion = '3.14.0-rc3';
 
-$dailyReleaseDateLinux = '20241227';
+$dailyReleaseDateLinux = '20241228';
 $dailyReleaseDateWindows = '20241228';
 $dailyReleaseDateMacos = '20241228';
 $dailyUrl = "https://download.nextcloud.com/desktop/daily/";
