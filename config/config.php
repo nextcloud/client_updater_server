59d373c283e97efc1d042c6748db7ebdbb79cd04
root <root@portal.portal.nextcloud.com>
2025-01-03 02:33:19 +0100

    Update daily Macos to 20250103
    Signed-off-by: root <root@portal.portal.nextcloud.com>

--- config/config.php
+++ config/config.php
@@ -17,7 +17,7 @@ $betaVersion = '3.14.0-rc3';
 
 $dailyReleaseDateLinux = '20250102';
 $dailyReleaseDateWindows = '20250103';
-$dailyReleaseDateMacos = '20250102';
+$dailyReleaseDateMacos = '20250103';
 $dailyUrl = "https://download.nextcloud.com/desktop/daily/";
 
 $stableVersionString = 'Nextcloud Client ' . $stableVersion;
