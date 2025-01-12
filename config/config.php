782eed6f7966c6cd82c81455e1cea5fede481e11
root <root@portal.portal.nextcloud.com>
2025-01-12 02:24:14 +0100

    Update daily Windows to 20250112
    Signed-off-by: root <root@portal.portal.nextcloud.com>

--- config/config.php
+++ config/config.php
@@ -16,7 +16,7 @@ $betaVersionInternal = '3.13.83';
 $betaVersion = '3.14.0-rc3';
 
 $dailyReleaseDateLinux = '20250111';
-$dailyReleaseDateWindows = '20250111';
+$dailyReleaseDateWindows = '20250112';
 $dailyReleaseDateMacos = '20250111';
 $dailyUrl = "https://download.nextcloud.com/desktop/daily/";
 
