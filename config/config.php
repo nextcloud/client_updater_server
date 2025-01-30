a0a0cb93acfbfa7517a4434abc02edcae86989b8
root <root@portal.portal.nextcloud.com>
2025-01-30 04:01:32 +0100

    Update daily Linux to 20250130
    Signed-off-by: root <root@portal.portal.nextcloud.com>

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
 //
 // daily
 //
-$dailyReleaseDateLinux = '20250116';
+$dailyReleaseDateLinux = '20250130';
 $dailyReleaseDateWindows = '20250130';
 $dailyReleaseDateMacos = '20250130';
 $dailyUrl = "https://download.nextcloud.com/desktop/daily/";
