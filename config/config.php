b9aa32159ad832148f4cb8663d8f702085ba8b72
root <root@portal.portal.nextcloud.com>
2025-09-20 02:29:10 +0200

    Update daily Windows to 20250920
    Signed-off-by: root <root@portal.portal.nextcloud.com>

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 // daily
 //
 $dailyReleaseDateLinux = '20250919';
-$dailyReleaseDateWindows = '20250919';
+$dailyReleaseDateWindows = '20250920';
 $dailyReleaseDateMacos = '20250919';
 
 //
