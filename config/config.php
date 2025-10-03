94df76af4b60ec33d13b03ed3cdd955aec9c53df
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2025-10-03 16:53:47 +0200

    chore: publish 4.0.0 rc1 beta release
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- config/config.php
+++ config/config.php
@@ -17,13 +17,13 @@ $dailyReleaseDateMacos = '20250922';
 // beta
 // should point to stable, once stable is released
 //
-$betaReleaseDate = '2025-09-16 16:00';
-$betaVersionInternal = '3.17.2'; // short string
-$betaVersion = '3.17.2'; // long string like '3.16.0-rc3' used to hide the cryptical subversion like .58 from the user;
-$betaVersionSignature = 'tu43rluaUMB8dqRLbAqyU/hApu+TOsDfblsnYH84EJ9mT4LOyJnjNyyu4N7xgWtTOrayayN7NIhdwZq9rWaaCw==';
-$betaVersionLength = 332685581;
-$betaVersionFileProviderSignature = '71a2XwjBRVfbQ8l1PV7rqeI8nzgpuyc68DVvnW06REgI4RX/nFS1iRrVvlNub+tgebWSayk3mWf3NZL2dU9aAA==';
-$betaVersionFileProviderLength = 374510489;
+$betaReleaseDate = '2025-10-03 17:00';
+$betaVersionInternal = '3.17.81'; // short string
+$betaVersion = '4.0.0-rc1'; // long string like '3.16.0-rc3' used to hide the cryptical subversion like .58 from the user;
+$betaVersionSignature = 'LPEoXIFMg7A917VZ11YWGzb0FcyXVVp903BK/QaT22BiAGBnJDAUspD6z+qOdkcWj95Z61bP32dLlzZNLFX7AQ==';
+$betaVersionLength = 341982826;
+$betaVersionFileProviderSignature = 'Y3nPgafLLPKJrRbWSJIJh7Oe/rnYH8g8FF9E3uRHFmS5oW61HwLKt4j2q6VeaVa4brtJ71jTi/Mv4Ze65ohiBQ==';
+$betaVersionFileProviderLength = 383794901;
 
 //
 // stable
