3f763868d8b056f9e4296c3a5e0c2d422dc79d57
root <root@portal.portal.nextcloud.com>
2026-03-02 03:19:20 +0100

    Update daily Macos to 20260302
    Signed-off-by: root <root@portal.portal.nextcloud.com>

--- config/config.php
+++ config/config.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
 //
 $dailyReleaseDateLinux = '20260301';
 $dailyReleaseDateWindows = '20260302';
-$dailyReleaseDateMacos = '20260301';
+$dailyReleaseDateMacos = '20260302';
 
 //
 // beta
