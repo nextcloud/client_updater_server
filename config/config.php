51718ab2483f67f4f877308d5fdd67175748cced
root <root@portal.portal.nextcloud.com>
2026-05-13 02:32:15 +0200

    Update daily Windows to 20260513
    Signed-off-by: root <root@portal.portal.nextcloud.com>

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 // daily
 //
 $dailyReleaseDateLinux = '20260512';
-$dailyReleaseDateWindows = '20260511';
+$dailyReleaseDateWindows = '20260513';
 $dailyReleaseDateMacos = '20260511';
 
 //
