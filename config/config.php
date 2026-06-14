ebde5dea6cd31e1858204c870631a5e84019b58f
root <root@portal.portal.nextcloud.com>
2026-06-14 03:58:11 +0200

    Update daily Linux to 20260614
    Signed-off-by: root <root@portal.portal.nextcloud.com>

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
 //
 // daily
 //
-$dailyReleaseDateLinux = '20260613';
+$dailyReleaseDateLinux = '20260614';
 $dailyReleaseDateWindows = '20260614';
 $dailyReleaseDateMacos = '20260520';
 
