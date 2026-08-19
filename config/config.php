4e45d73f747a44fde1a1fd6b9a7a6c28ca8d2486
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2026-08-19 11:35:26 +0200

    chore: publish 34.0.2 to beta and stable channels
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- config/config.php
+++ config/config.php
@@ -17,11 +17,11 @@ $dailyReleaseDateMacos = '20260717';
 // beta
 // should point to stable, once stable is released
 //
-$betaReleaseDate = '2026-08-05 09:00';
-$betaVersionInternal = '34.0.1'; // short string
-$betaVersion = '34.0.1'; // long string like '3.16.0-rc3' used to hide the cryptical subversion like .58 from the user;
-$betaVersionSignature = 'eqVfDIYwicoeyyegCKv5/JFbzikNOFNHjopDbYYlK8jzpzjKMtG4ZIwCMfUNggDnXUtmPzaAJXzJzGveUDHQBA==';
-$betaVersionLength = 148431079;
+$betaReleaseDate = '2026-08-19 12:00';
+$betaVersionInternal = '34.0.2'; // short string
+$betaVersion = '34.0.2'; // long string like '3.16.0-rc3' used to hide the cryptical subversion like .58 from the user;
+$betaVersionSignature = 'L2gYEuKi/er/CN4dbIPFnPqnxAgbgBOBuIt+nRMOUAiXOeT19CwgiuW4B31OPWo61aorU1TbRXWQi8Z4/OhMCA==';
+$betaVersionLength = 153013529;
 
 //
 // stable Qt6.9 (macOS 11 / 12 — not compatible with Qt6.10)
@@ -37,10 +37,10 @@ $stableQt69VersionFileProviderLength = 373257027;
 //
 // stable
 //
-$stableReleaseDate = '2026-08-05 09:00';
-$stableVersion = '34.0.1';
-$stableVersionSignature = 'eqVfDIYwicoeyyegCKv5/JFbzikNOFNHjopDbYYlK8jzpzjKMtG4ZIwCMfUNggDnXUtmPzaAJXzJzGveUDHQBA==';
-$stableVersionLength = 148431079;
+$stableReleaseDate = '2026-08-19 12:00';
+$stableVersion = '34.0.2';
+$stableVersionSignature = 'L2gYEuKi/er/CN4dbIPFnPqnxAgbgBOBuIt+nRMOUAiXOeT19CwgiuW4B31OPWo61aorU1TbRXWQi8Z4/OhMCA==';
+$stableVersionLength = 153013529;
 
 //
 // enterprise 
